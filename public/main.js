0e0b5ff1acc231b5327e67e7d929185699c2510b
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-06 12:43:48 -0400

    cleanup frontend js

--- public/main.js
+++ public/main.js
@@ -1,35 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-
-
-
-
-//Front Page
-var fullstats = document.getElementById("fullstatistics");
-if (fullstats !== null){
-  var jsonObject = JSON.parse(fullstats.textContent);
-  fullstats.innerHTML = "";
-  jsonObject.map((user) => {
+  //Profile Page
+  var profilepage = document.getElementById("profilesnippets");
+  if (profilepage !== null){
+    var arrayOfSnips = JSON.parse(profilepage.textContent);
+    profilepage.innerHTML = "";
     var newliteral = document.createElement("div");
-    newliteral.setAttribute("class", "playerstats");
-    let holder = `
-      <h2><a href="/profile${user.username}">${user.username}</a></h2>
-      <p>Games: ${user.games}  Wins: ${user.wins} Losses: ${user.losses}</p>
-      <p>Avg Word Length: ${(user.wordlengths.reduce((a,b) => a+b, 0))/user.wordlengths.length}</p>
-      <p>Avg Time: ${timecalc((user.times.reduce((a,b) => a+b, 0))/user.times.length)}</p>
-    `;
-    newliteral.innerHTML = holder;
-    fullstats.appendChild(newliteral);
-  })
-}
-//Profile Page
-var profilepage = document.getElementById("profilesnippets");
-if (profilepage !== null){
-  var arrayOfSnips = JSON.parse(profilepage.textContent);
-  profilepage.innerHTML = "";
-  var newliteral = document.createElement("div");
-  newliteral.setAttribute("class", "personalsnips");
-  arrayOfSnips.map((obj, index) => {
+    newliteral.setAttribute("class", "personalsnips");
+    arrayOfSnips.map((obj, index) => {
+      let holder = `
+        <a href="/snippetview${obj._id}/"><h2>Title: ${obj.title}</h2></a>
+        <h3>Notes: ${obj.notes}</h3>
+        <h4>Snippet: ${obj.codesnippet}</h4>
+        <h5>Language: ${obj.language}</h5>
+        <h5>Created: ${obj.createdAt}</h5>
+        <h5>Updated: ${obj.updatedAt}</h5>
+        <h5>Tags: ${obj.tags}</h5>
+      `;
+      newliteral.innerHTML += holder;
+    })
+    profilepage.appendChild(newliteral);
+  }
+  //Single Snippet Page
+  var specificsnippets = document.getElementById("specificsnippets");
+  if (specificsnippets !== null){
+    var obj = JSON.parse(specificsnippets.textContent);
+    specificsnippets.innerHTML = "";
+    var newliteral = document.createElement("div");
+    newliteral.setAttribute("class", "personalsnips");
     let holder = `
       <a href="/snippetview${obj._id}/"><h2>Title: ${obj.title}</h2></a>
       <h3>Notes: ${obj.notes}</h3>
@@ -40,33 +37,6 @@ if (profilepage !== null){
       <h5>Tags: ${obj.tags}</h5>
     `;
     newliteral.innerHTML += holder;
-  })
-  profilepage.appendChild(newliteral);
-}
-//Single Snippet Page
-var specificsnippets = document.getElementById("specificsnippets");
-if (specificsnippets !== null){
-  var obj = JSON.parse(specificsnippets.textContent);
-  specificsnippets.innerHTML = "";
-  var newliteral = document.createElement("div");
-  newliteral.setAttribute("class", "personalsnips");
-  let holder = `
-    <a href="/snippetview${obj._id}/"><h2>Title: ${obj.title}</h2></a>
-    <h3>Notes: ${obj.notes}</h3>
-    <h4>Snippet: ${obj.codesnippet}</h4>
-    <h5>Language: ${obj.language}</h5>
-    <h5>Created: ${obj.createdAt}</h5>
-    <h5>Updated: ${obj.updatedAt}</h5>
-    <h5>Tags: ${obj.tags}</h5>
-  `;
-  newliteral.innerHTML += holder;
-  specificsnippets.appendChild(newliteral);
-}
-
-
-
-
-
-
-
+    specificsnippets.appendChild(newliteral);
+  }
 });
